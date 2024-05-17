86d99eda6b3b801e55fe872993a7e10c25f702f9
sp185372 <sp185372@WIN-BQ2NPX3>
2024-05-17 19:52:42 +0530

    Added Jokes Service.

---
+++ src/main/java/guru/springframework/spring5jokesappv2/services/JokesServiceImpl.java
@@ -0,0 +1,20 @@
+package guru.springframework.spring5jokesappv2.services;
+
+import org.springframework.stereotype.Service;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+
+@Service
+public class JokesServiceImpl implements JokesService {
+
+	private final ChuckNorrisQuotes chuckNorrisQuotes;
+	
+	public JokesServiceImpl() {
+		this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+	}
+	
+	public String getJoke() {
+		return chuckNorrisQuotes.getRandomQuote();
+	}
+
+}
